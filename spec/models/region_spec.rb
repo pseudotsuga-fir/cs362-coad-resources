a8c73450a51245aab6ac8253d15ddbde56e15bc8
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-01-25 10:33:23 -0800

    Tested region methods. References: #8

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+  describe "#to_s" do
+    it "returns the name" do
+      name = 'Mt. Hood'
+      region = Region.new(name: name)
+      expect(region.to_s).to eq(name)
+    end
+  end
+  
 end
