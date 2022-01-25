323f2bda7e21074ee538570ce9d8595de652c281
Baleria <breyes815@gmail.com>
2022-01-25 10:31:43 -0800

    Region attributes tested  Reference #7

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
+    describe "attributes" do 
+        it "has a name" do 
+            region = Region.new
+            expect(region).to respond_to(:name)
+        end
+    end
+
 end
