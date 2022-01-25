f04741c4c426ab204de3b91f578aa64125967d78
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-01-25 10:48:22 -0800

    Region models spec now tests name is valid. References: #7

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,10 +3,23 @@ require 'rails_helper'
 RSpec.describe Region, type: :model do
   
   describe "attributes" do 
-      it "has a name" do 
+    it "has a name" do 
       region = Region.new
       expect(region).to respond_to(:name)
     end
+
+    it "has a non empty name" do
+      name = ""
+      region = Region.new(name: name)
+      expect(region).to_not be_valid
+    end
+
+    it "has a length of valid name" do
+      name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      region = Region.new(name: name)
+      expect(region).to_not be_valid
+    end
+
   end
 
   describe "#to_s" do
