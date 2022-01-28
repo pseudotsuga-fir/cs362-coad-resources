3991e5e8ae54b386db420b52de3d42675a32d3d5
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-01-27 16:29:22 -0800

    neccessary user attributes under test. References: #14

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+    describe "attributes" do
+
+        it "has an email" do
+            expect(User.new).to respond_to(:email)
+        end
+
+        it "has a role" do
+            expect(User.new).to respond_to(:role)
+        end
+    end
+
 end
