6b9fa16c32868c14bf519c36612af736138d5515
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-01-31 11:27:52 -0800

    fixed 4 space indentations. References: #20

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   let(:user) {
     User.new(
       email: "test@test.org",
@@ -9,54 +8,52 @@ RSpec.describe User, type: :model do
     )
   }
 
-    describe "attributes" do
-        it "has an email" do
-          expect(User.new).to respond_to(:email)
-        end
-
-        it "has a role" do
-          expect(User.new).to respond_to(:role)
-        end   
+  describe "attributes" do
+    it "has an email" do
+      expect(User.new).to respond_to(:email)
     end
 
-    describe "validators" do
-      it "validates presence of email" do
-        should validate_presence_of(:email)
-      end
+    it "has a role" do
+      expect(User.new).to respond_to(:role)
+    end
+  end
 
-      it "validates presence of password" do
-        should validate_presence_of(:password)
-      end
+  describe "validators" do
+    it "validates presence of email" do
+      should validate_presence_of(:email)
+    end
 
-      it "validate length of email" do
-        should validate_length_of(:email).
-        is_at_least(1).is_at_most(255)
-      end
+    it "validates presence of password" do
+      should validate_presence_of(:password)
+    end
 
-      it "validate uniqueness of email" do
-        should validate_uniqueness_of(:email).case_insensitive
-      end
+    it "validate length of email" do
+      should validate_length_of(:email).
+      is_at_least(1).is_at_most(255)
+    end
 
-      it "rejects emails without @" do
-        user.email = "test.com"
-        expect(user).to be_invalid
-      end
-  
-      it "rejects emails without domain" do
-        user.email = "test@test"
-        expect(user).to be_invalid
-      end
+    it "validate uniqueness of email" do
+      should validate_uniqueness_of(:email).case_insensitive
+    end
 
-      it "accepts valid emails" do
-        user.email = "test@test.xyz"
-        expect(user).to be_valid
-      end
+    it "rejects emails without @" do
+      user.email = "test.com"
+      expect(user).to be_invalid
+    end
 
-      it "validates length of password" do
-        should validate_length_of(:password).
+    it "rejects emails without domain" do
+      user.email = "test@test"
+      expect(user).to be_invalid
+    end
+
+    it "accepts valid emails" do
+      user.email = "test@test.xyz"
+      expect(user).to be_valid
+    end
+
+    it "validates length of password" do
+      should validate_length_of(:password).
         is_at_least(7).is_at_most(255)
-      end
-
     end
-
+  end
 end
