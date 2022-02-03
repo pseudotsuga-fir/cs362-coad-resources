3fdcf5e32061aa23e9979076e81c8646ccbe22fb
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-02-03 11:45:09 -0800

    Resource Category attributes tested. References: #19

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+  let(:resource_category) { build(:resource_category) }
 
+  describe "atttributes" do
+    it "has a name" do
+      expect(resource_category).to respond_to(:name)
+    end
+
+    it "has an active status" do
+      expect(resource_category).to respond_to(:active)
+    end
+  end
 end
