d6cedf79d331ccf5c239f8eac53b8620d08b3cdd
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-02-26 12:38:32 -0800

    users factory now has associations with organization
    References: #50

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
+    association :organization, factory: :organization
+    
     sequence(:email) { |n| "fake#{n}@fake.com" }
     password { "I am a password" }
   end
