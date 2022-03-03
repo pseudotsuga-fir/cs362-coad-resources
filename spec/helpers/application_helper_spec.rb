7bb300bae08bc3e4eb524005f9086882b3ce352a
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-02 16:48:56 -0800

    full_title helper method tested.
    References: #53

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+  describe "#full_title" do
+    it "returns the base title when not given an argument" do
+      expect(helper.full_title).to eq('Disaster Resource Network')
+    end
 
+    it "returns a combination of passed in argument and base title" do
+      expect(helper.full_title('FakeTitle')).to eq ('FakeTitle | Disaster Resource Network')
+    end
+  end
 end
