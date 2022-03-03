8ca7f47006a9814ea14d77c276bfd0723b407ab6
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-03 10:25:11 -0800

    dashboard helper dashboard_for tested
    References: #54

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -11,5 +11,32 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe DashboardHelper, type: :helper do
+  describe "#dashboard_for" do
+    it "gives admins the admin dashbaord" do
+      user = build(:user)
+      user.role = :admin
+      expect(helper.dashboard_for(user)).to eq('admin_dashboard')
+    end
 
+    it "gives submitted organization users the organization submitted dashbaord" do
+      user = build(:user)
+      user.role = :organization
+      user.organization.status = :submitted
+      expect(helper.dashboard_for(user)).to eq('organization_submitted_dashboard')
+    end
+
+    it "gives approved organization the organization approved dashbaord" do
+      user = build(:user)
+      user.role = :organization
+      user.organization.status = :approved
+      expect(helper.dashboard_for(user)).to eq('organization_approved_dashboard')
+    end
+
+    it "gives create application dashboard for other users" do
+      user = build(:user)
+      user.role = :organization
+      user.organization.status = :rejected
+      expect(helper.dashboard_for(user)).to eq('create_application_dashboard')
+    end
+  end
 end
