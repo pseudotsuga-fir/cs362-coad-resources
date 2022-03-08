b1f97846aa0c51a2fb0228e62a563e68b8aa17e2
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-08 10:57:36 -0800

    reject organization feature tested
    References: #57

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+  it "shows a success message" do
+    admin = create(:user, :admin)
+    admin.confirm
+    log_in_as(admin)
 
+    visit "/organizations"
+    click_link("Pending")
+    click_on("Review")
+    click_on("Reject")
+    expect(page).to have_text("has been rejected.")
+  end
 end
