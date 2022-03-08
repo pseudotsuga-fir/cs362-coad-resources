7505c9185a94df1122ecca4a5f8c3db968a5f4db
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-08 10:53:24 -0800

    renamed organization it block
    Referenecs: #55

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-  it "gives success?" do
+  it "shows a success message" do
     admin = create(:user, :admin)
     admin.confirm
     log_in_as(admin)
