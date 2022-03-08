7bcc41f81c396520cbffe1b8ee1817c4e5f3579c
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-08 15:03:28 -0800

    admin deletes region tested
    References: #61

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  it "######################################" do
+    admin = create(:user, :admin)
+    admin.confirm
+    log_in_as(admin)
 
+    visit "/dashboard"
+    click_on("Regions")
+    click_on("Add Region")
+    region_name = "FakestRegionEver"
+    fill_in("Name", with: region_name)
+    click_on("Add Region")
+
+    click_on(region_name)
+    click_on("Delete")
+
+    expect(page).to have_text("Region #{region_name} was deleted.")
+  end
 end
