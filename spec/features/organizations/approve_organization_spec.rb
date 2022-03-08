099611adbc93352a4e1260e36ae2287a7d08d4f9
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-08 10:52:03 -0800

    approve organization feature tested
    References: #55

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  it "gives success?" do
+    admin = create(:user, :admin)
+    admin.confirm
+    log_in_as(admin)
 
+    visit "/organizations"
+    click_link("Pending")
+    click_on("Review")
+    click_on("Approve")
+    expect(page).to have_text("has been approved.")
+
+  end
 end
