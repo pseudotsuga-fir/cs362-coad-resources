1741ff618dd5db1b711bb5164965967ec1cace3e
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-08 11:37:59 -0800

    create region feature tested
    References: #60

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+  it "tells admin creation was successful" do
+    admin = create(:user, :admin)
+    admin.confirm
+    log_in_as(admin)
+
+    visit "/dashboard"
+    click_on("Regions")
+    click_on("Add Region")
+    region_name = "FakestRegionEver"
+    fill_in("Name", with: region_name)
+    click_on("Add Region")
+    expect(page).to have_text("Region successfully created.")
+  end
 
 end
