d2cb23c407295473c8fed6fc4bc07cbb8f547b3a
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-08 11:08:19 -0800

    create organization feature tested
    References: #56

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
-
+  it "CHANFGE ME!" do    
+    user = create(:user, :organization)
+    user.confirm
+    log_in_as(user)
+  
+    visit "/organizations/new"
+    expect(page).to have_text("Thank you for applying.")
+  end
 end
