dcdf6d21fb6c310bb6d5f72db7753c7857950718
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-11 16:29:27 -0800

    updated UserMailer to only send emails in production
    References: #64

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def new_organization_application
     @to = params[:to]
     @new_organization = params[:new_organization]
-    if Rails.env.production? || Rails.env.test?
+    if Rails.env.production?
       mail(to: @to, subject: 'New Organization Application Pending')
     else
       false
