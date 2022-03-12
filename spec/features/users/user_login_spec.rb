af0a0da11e95a57ee4ab6dc6e2b08e6fa41cabd5
Baleria <breyes815@gmail.com>
2022-03-12 13:55:17 -0800

    user_login_spec almost fully tested. Having trouble getting passed Sign in button.

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-    
+    scenario "shows that user has logged in" do
+        visit "/login"
+        fill_in "Email address", with: "test@test.com"
+        fill_in "Password", with: "password"
+        click_on "commit"
+        # expect(page).to have_text("Dashboard")
+    end
+
 end
