8ef48698b3e0bf69521bc94a248c61fc25268902
Baleria <breyes815@gmail.com>
2022-03-12 13:49:07 -0800

    UML diagram added.

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-    scenario "shows that user has logged in" do
-        visit "/login"
-        fill_in "Email address", with: "breyes815@gmail.com"
-        fill_in "Password", with: "Baboon101"
-        click_on "commit"
-        expect(page).to have_text("Dashboard")
-    end
-
+    
 end
