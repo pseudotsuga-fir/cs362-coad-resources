a672f4a359a023f87790247e73de30622484634d
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-03-11 16:48:46 -0800

    organization updating featuer tested
    References: #58

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+  it "shows user their organization afterward" do
+    user = create(:user, :organization)
+    user.confirm
+    log_in_as(user)
+    user.organization.approve
+    user.organization.save!
 
+    visit "/dashboard"
+    
+    click_on("Edit Organization")
+    click_on("Update Organization")
+    expect(page).to have_current_path("/organizations/#{user.organization.id}")
+  end
 end
