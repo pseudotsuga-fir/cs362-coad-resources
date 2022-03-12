490ba4aa20de50b850b052928fe1ac0ce8ad881a
Baleria <breyes815@gmail.com>
2022-03-12 14:14:12 -0800

    Organization cdescription added.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
+# The Organization class is used to group users. The users can then be assigned tickets.
+
 class Organization < ApplicationRecord
 
   attr_accessor :agreement_one, :agreement_two, :agreement_three, :agreement_four, :agreement_five, :agreement_six, :agreement_seven, :agreement_eight
